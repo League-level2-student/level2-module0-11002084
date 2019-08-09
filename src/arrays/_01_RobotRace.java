9fb7dc4be2cc7f48bc061a44944ea553a2831bd1
League Student <league@iMac-37.attlocal.net>
2019-08-09 16:28:07 -0700

    Robot Race, Hidden Button, Visual Array Sorter (IP)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -19,57 +19,57 @@ public class _01_RobotRace {
 
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-//		robots[0].setX(100);
-//		robots[0].setY(600);
-//		robots[1].setX(200);
-//		robots[1].setY(600);
-//		robots[2].setX(300);
-//		robots[2].setY(600);
-//		robots[3].setX(400);
-//		robots[3].setY(600);
-//		robots[4].setX(500);
-//		robots[4].setY(600);
-//		robots[5].setX(600);
-//		robots[5].setY(600);
-//		robots[6].setX(700);
-//		robots[6].setY(600);
-//		robots[7].setX(800);
-//		robots[7].setY(600);
+		robots[0].setX(100);
+		robots[0].setY(600);
+		robots[1].setX(200);
+		robots[1].setY(600);
+		robots[2].setX(300);
+		robots[2].setY(600);
+		robots[3].setX(400);
+		robots[3].setY(600);
+		robots[4].setX(500);
+		robots[4].setY(600);
+		robots[5].setX(600);
+		robots[5].setY(600);
+		robots[6].setX(700);
+		robots[6].setY(600);
+		robots[7].setX(800);
+		robots[7].setY(600);
 
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
-//
-//		int lowestY = 1000;
-//
-//		while (lowestY > 50) {
-//			for (int i = 0; i < 8; i++) {
-//				Random rand = new Random();
-//				int num = rand.nextInt(50);
-//				robots[i].setY(robots[i].getY() - num);
-//				if (robots[i].getY() < lowestY) {
-//					lowestY = robots[i].getY();
-//				}
-//				if (lowestY <= 50) {
-//					for (int e = 0; e < 8; e++) {
-//						robots[e].hide();
-//					}
-//					robots[i].setX(450);
-//					robots[i].setY(300);
-//					robots[i].sleep(3000);
-//					robots[i].show();
-//					System.out.println("Robot "+ i + " is the winner!");
-//					robots[i].setSpeed(20);
-//					while (lowestY <= 50) {
-//						robots[i].turn(360);
-//						robots[i].sparkle();
-//						robots[i].turn(360);
-//						robots[i].unSparkle();
-//					}
-//				}
-//
-//				robots[i].sleep(1000);
-//			}
-//		}
+
+		int lowestY = 1000;
+
+		while (lowestY > 50) {
+			for (int i = 0; i < 8; i++) {
+				Random rand = new Random();
+				int num = rand.nextInt(50);
+				robots[i].setY(robots[i].getY() - num);
+				if (robots[i].getY() < lowestY) {
+					lowestY = robots[i].getY();
+				}
+				if (lowestY <= 50) {
+					for (int e = 0; e < 8; e++) {
+						robots[e].hide();
+					}
+					robots[i].setX(450);
+					robots[i].setY(300);
+					robots[i].sleep(3000);
+					robots[i].show();
+					System.out.println("Robot " + i + " is the winner!");
+					robots[i].setSpeed(20);
+					while (lowestY <= 50) {
+						robots[i].turn(360);
+						robots[i].sparkle();
+						robots[i].turn(360);
+						robots[i].unSparkle();
+					}
+				}
+
+				robots[i].sleep(200);
+			}
+		}
 
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
@@ -80,26 +80,34 @@ public class _01_RobotRace {
 
 		// 9. make the robots race around a circular track.
 
-		robots[0].setX(100);
-		robots[0].setY(300);
-		robots[1].setX(100);
-		robots[1].setY(300);
-		robots[2].setX(100);
-		robots[2].setY(300);
-		robots[3].setX(100);
-		robots[3].setY(300);
-		robots[4].setX(100);
-		robots[4].setY(300);
-		robots[5].setX(100);
-		robots[5].setY(300);
-		robots[6].setX(100);
-		robots[6].setY(300);
-		robots[7].setX(100);
-		robots[7].setY(300);
+		robots[0].setX(101);
+		robots[0].setY(301);
+		robots[1].setX(101);
+		robots[1].setY(301);
+		robots[2].setX(101);
+		robots[2].setY(301);
+		robots[3].setX(101);
+		robots[3].setY(301);
+		robots[4].setX(101);
+		robots[4].setY(301);
+		robots[5].setX(101);
+		robots[5].setY(301);
+		robots[6].setX(101);
+		robots[6].setY(301);
+		robots[7].setX(101);
+		robots[7].setY(301);
+		robots[0].setAngle(1);
+		robots[1].setAngle(1);
+		robots[2].setAngle(1);
+		robots[3].setAngle(1);
+		robots[4].setAngle(1);
+		robots[5].setAngle(1);
+		robots[6].setAngle(1);
+		robots[7].setAngle(1);
 
-		int lowestX = 1000;
+		double lowestX = 1000;
 
-		while (lowestX == 1000) {
+		while (lowestX > 100) {
 			for (int i = 0; i < 8; i++) {
 				Random rand = new Random();
 				int num = rand.nextInt(10);
@@ -107,27 +115,27 @@ public class _01_RobotRace {
 					robots[i].setSpeed(1500);
 					robots[i].turn(1);
 					robots[i].move(5);
+					if (robots[i].getX() < lowestX) {
+						lowestX = robots[i].getX();
+					}
 				}
-//					if (robots[i].getX() < lowestX) {
-//						lowestX = robots[i].getY();
-//					}
-//				if (lowestX <= 50) {
-//					for (int e = 0; e < 8; e++) {
-//						robots[e].hide();
-//					}
-//					robots[i].setX(450);
-//					robots[i].setY(300);
-//					robots[i].sleep(3000);
-//					robots[i].show();
-//					System.out.println("Robot " + i + " is the winner!");
-//					robots[i].setSpeed(20);
-//					while (lowestX <= 50) {
-//						robots[i].turn(360);
-//						robots[i].sparkle();
-//						robots[i].turn(360);
-//						robots[i].unSparkle();
-//					}
-//				}
+				if (lowestX <= 100.99) {
+					for (int e = 0; e < 8; e++) {
+						robots[e].hide();
+					}
+					robots[i].setX(450);
+					robots[i].setY(300);
+					robots[i].sleep(3000);
+					robots[i].show();
+					System.out.println("Robot " + i + " is the winner!");
+					robots[i].setSpeed(20);
+					while (lowestX <= 100.99) {
+						robots[i].turn(360);
+						robots[i].sparkle();
+						robots[i].turn(360);
+						robots[i].unSparkle();
+					}
+				}
 
 				robots[i].sleep(200);
 			}
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -9,63 +9,111 @@ package arrays;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-	
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_FindTheHiddenButton implements ActionListener{
+public class _02_FindTheHiddenButton implements ActionListener {
 	JFrame window;
 	JPanel panel;
-	
-	//1. create an array of JButtons. Don't initialize it yet.
-	
-	//2 create an int variable called hiddenButton
-	
+
+	// 1. create an array of JButtons. Don't initialize it yet.
+
+	JButton[] buttons;
+
+	// 2 create an int variable called hiddenButton
+
+	int hiddenButton;
+
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
-	
+
 	public void start() {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//3.  Ask the user to enter a positive number and convert it to an int
-		
-		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
-		//5. Make a for loop to iterate through the JButton array
-			//6. initialize each JButton in the array
-			//7. add the ActionListener to each JButton
-			//8. add each JButton to the panel
-		
-		//9 add the panel to the window
-		
-		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
-		//11. set the JFrame to visible.
-		
-		//12. Give the user the instructions for the game.
-		
-		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
-		
-		//14. Set the text of the JButton located at hiddenButton to  "ME"
 
-		//15. Use Thread.sleep(1000); to pause the program.
-		//    Surround it with a try/catch - use Eclipse helper for this
-		
-		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+		// 3. Ask the user to enter a positive number and convert it to an int
+
+		String inputValue = JOptionPane.showInputDialog(
+				"Enter a positive number. ONLY POSITIVE. If you enter something else, there will be lots of crying and sadness.");
+		int intInputValue = Integer.parseInt(inputValue);
+		if (intInputValue < 0) {
+			JOptionPane.showMessageDialog(null,
+					"You dummy, you numbskull, you absolute buffoon. You had one job. ONE JOB.");
+		}
+
+		// 4. Initialize the array of JButtons to be the size of the int created in step
+
+		buttons = new JButton[intInputValue];
+
+		// 5. Make a for loop to iterate through the JButton array
+
+		for (int i = 0; i < buttons.length - 1; i++) {
+			buttons[i] = new JButton();
+			buttons[i].addActionListener(this);
+			panel.add(buttons[i]);
+		}
+
+		window.add(panel);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setVisible(true);
+		// 6. initialize each JButton in the array
+		// 7. add the ActionListener to each JButton
+		// 8. add each JButton to the panel
+
+		// 9 add the panel to the window
+
+		// 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+
+		// 11. set the JFrame to visible.
+
+		// 12. Give the user the instructions for the game.
+
+		JOptionPane.showMessageDialog(null,
+				"Oh boy. A game. A button will show the word 'E'. Then the program will pause and you will have to click the correct button after it unpauses. Have fun.");
+
+		// 13. initialize the hiddenButton variable to a random number less than the int
+		// created int step 3
+		Random rand = new Random();
+
+		hiddenButton = rand.nextInt(intInputValue);
+
+		// 14. Set the text of the JButton located at hiddenButton to "ME"
+
+		buttons[hiddenButton].setText("E");
+
+		// 15. Use Thread.sleep(1000); to pause the program.
+		// Surround it with a try/catch - use Eclipse helper for this
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// 16. Set the text of the JButton located at hiddenButton to be blank.
+
+		buttons[hiddenButton].setText("");
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton buttonClicked = (JButton)e.getSource();
-		
-		//17. if the hiddenButton is clicked, tell the user that they win.
-		
-		//18. else tell them to try again
+		JButton buttonClicked = (JButton) e.getSource();
+
+		// 17. if the hiddenButton is clicked, tell the user that they win.
+
+		if (buttonClicked == buttons[hiddenButton]) {
+			JOptionPane.showMessageDialog(null, "Oh boy. You won. Good for you.");
+		} else {
+			JOptionPane.showMessageDialog(null, "*Visible Disappointment* Try again.");
+		}
+
+		// 18. else tell them to try again
 	}
 }
